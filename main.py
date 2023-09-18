cebe83dc1699a461534a44a10f47156f75bb58f5
Yasin Gul <yasing@MacBook-Air-som-tillhor-Yasin.local>
2023-09-18 10:38:33 +0200

    Börjar på Programmet

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ class BankKonto:
         self.pin = pin
         self.saldo = saldo
 
+    def insattning(self, belopp):
+        self.saldo += belopp
+        return self.saldo
+
+    def uttag(self, belopp):
+        if belopp > self.saldo:
+            return "Inte tillräckligt med pengar på kontot!"
+        self.saldo -= belopp
+        return self.saldo
+
+    def visa_saldo(self):
+        return self.saldo
