2f4e508b3e942ff2e3616d14d539a47e3d939f20
Yasin Gul <yasing@MacBook-Air-som-tillhor-Yasin.local>
2023-09-18 12:54:39 +0200

    implementerar en mer objekt orienterad lösning

---
+++ bankkonto.py
@@ -0,0 +1,18 @@
+class BankKonto:
+    def __init__(self, namn, pin, saldo=0):
+        self.namn = namn
+        self.pin = pin
+        self.saldo = saldo
+
+    def insattning(self, belopp):
+        self.saldo += belopp
+        return self.saldo
+
+    def uttag(self, belopp):
+        if belopp > self.saldo:
+            return "Inte tillräckligt med pengar på kontot!"
+        self.saldo -= belopp
+        return self.saldo
+
+    def visa_saldo(self):
+        return self.saldo
