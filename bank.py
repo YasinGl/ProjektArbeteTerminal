510043fa223e0c2bfb198d80caa08ee5875c07ee
Yasin Gul <yasing@MacBook-Air-som-tillhor-Yasin.local>
2023-09-18 14:12:59 +0200

    Kommenterar all kod

--- bank.py
+++ bank.py
@@ -1,12 +1,16 @@
-from bankkonto import BankKonto
+from bankkonto import BankKonto # Importera BankKonto-klassen från bankkonto-modulen.
 
-class Bank:
+
+class Bank: # Definiera Bank-klassen.
+    # Initiera Bank-objektet med en tom dictionary för att lagra bankkonton.
     def __init__(self):
         self.konton = {}
 
-    def skapa_konto(self, namn, pin):
+    def skapa_konto(self, namn, pin):     # Funktion för att skapa ett nytt konto.
+        # Generera ett nytt kontonummer baserat på antalet befintliga konton.
         kontonummer = len(self.konton) + 1
         self.konton[kontonummer] = BankKonto(namn, pin)
+        # Returnera det nya kontonumret.
         return kontonummer
 
     def logga_in(self, kontonummer, pin):
