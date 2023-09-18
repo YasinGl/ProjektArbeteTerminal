936a2754d1eb7a136ff34dff78aa4631c67d32fa
Yasin Gul <yasing@MacBook-Air-som-tillhor-Yasin.local>
2023-09-18 12:54:59 +0200

    implementerar en mer objekt orienterad lösning

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-class BankKonto:
-    def __init__(self, namn, pin, saldo = 0):
-        self.namn = namn
-        self.pin = pin
-        self.saldo = saldo
-
-    def insattning(self, belopp):
-        self.saldo += belopp
-        return self.saldo
-
-    def uttag(self, belopp):
-        if belopp > self.saldo:
-            return "Inte tillräckligt med pengar på kontot!"
-        self.saldo -= belopp
-        return self.saldo
-
-    def visa_saldo(self):
-        return self.saldo
-
-
 class Bank:
     def __init__(self):
         self.konton = {}
